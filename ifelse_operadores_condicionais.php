75d7d5fa2a51993709be3f180c8230f0f1a981be
Welton Carvalho <weltoncarvalhoo@gmail.com>
2020-12-15 16:18:39 -0300

    If/else parte 1 - Praticando com operadores de comparação

---
+++ ifelse_operadores_condicionais.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Curso PHP</title>
+	</head>
+	<body>
+		<?php
+
+			//==
+			//===
+			//!= ou <>
+			//!==
+			//<
+			//>
+			//<=
+			//>=
+
+			if(15 <= 15)
+				echo 'Verdadeiro'
+
+		?>
+	</body>
+</html>
