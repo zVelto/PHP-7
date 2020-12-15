b95f3e520333e59ef4a6b40c24698ee4f711b206
Welton Carvalho <weltoncarvalhoo@gmail.com>
2020-12-14 22:38:54 -0300

    Comentários

---
+++ comentarios.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Curso PHP</title>
+	</head>
+	<body>
+		<!-- Comentário -->
+		<?php
+
+			// Comentário de uma linha
+			echo 'Comando echo<br/>';
+
+			# Comentário de uma linha
+			echo print'Comando print<hr/>';
+
+			/*
+				Comentário de múltiplas linhas.
+				Este tipo de comnetário permite que várias
+				linhas sejam comentadas ao mesmo tempo. 
+			*/
+		?>
+	</body>
+</html>
