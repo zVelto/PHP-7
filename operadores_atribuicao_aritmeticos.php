2581b23734d759480415edf147e4107163fa7bfc
Welton Carvalho <weltoncarvalhoo@gmail.com>
2020-12-17 22:22:09 -0300

    Operações aritméticas na atribuição de valores

---
+++ operadores_atribuicao_aritmeticos.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Curso PHP</title>
+	</head>
+	<body>
+		
+		<?php
+
+			// = 
+			// =, -, *, /, %
+
+			$x = 10;
+			$y = 15;
+
+			$x %= $y;
+
+			echo $x;
+
+		?>
+
+	</body>
+</html>
