d4a77ddd970e7541f933f751b41b900c28bbbc09
Welton Carvalho <weltoncarvalhoo@gmail.com>
2020-12-17 21:09:04 -0300

    Praticando com operadores aritméticos

---
+++ operadores_aritméticos.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Curso PHP</title>
+	</head>
+	<body>
+		
+		<?php
+
+			$num1 = 13;
+			$num2 = 4;
+
+			echo "A soma entre $num1 e $num2 é " . ($num1 + $num2);
+			echo '<br>';
+			echo "A diferença entre $num1 e $num2 é " . ($num1 - $num2);
+			echo '<br>';
+			echo "O produto entre $num1 e $num2 é " . ($num1 * $num2);
+			echo '<br>';
+			echo "A razão entre $num1 e $num2 é " . ($num1 / $num2);
+			echo '<br>';
+			echo "O módulo entre $num1 e $num2 é " . ($num1 % $num2);
+
+
+		?>
+
+	</body>
+</html>
