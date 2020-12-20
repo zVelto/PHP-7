6f80689e5d912e4bd4e94983c482e2b1d209f62f
Welton Carvalho <weltoncarvalhoo@gmail.com>
2020-12-19 23:48:22 -0300

    Funções nativas para tarefas matemáticas

---
+++ funcoes_matematicas.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Curso PHP</title>
+	</head>
+	<body>
+		
+		<?php
+
+			$num = 7.356;
+
+			//echo ceil($num); //arredonda para cima
+			//echo floor($num); // arredonda para baixo
+			//echo round($num); //arredonda com base na fração
+			//echo rand(10, 20); //gerar um valor aleatório
+			//echo '<br>';
+			//echo getrandmax();
+			echo sqrt(93);
+			
+		?>
+
+	</body>
+</html>
