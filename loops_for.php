6a89668b42411b79b06cc1b14abb12c92315053c
Welton Carvalho <weltoncarvalhoo@gmail.com>
2020-12-28 14:53:17 -0300

    Loops parte 3 - For

---
+++ loops_for.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Curso PHP</title>
+	</head>
+	<body>
+		
+		<?php  
+
+            /*
+            $x = 1;
+
+            while($s < 10) {
+
+            }
+
+            do {
+
+            } while($x < 10);
+            */
+
+            for($x = 10; $x > 0; $x--) {
+
+                echo "$x <br>";
+            }
+
+		?>
+
+	</body>
+</html>
