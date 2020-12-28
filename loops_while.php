c44ad174071c3c797fad3fb467ced7cf49820d58
Welton Carvalho <weltoncarvalhoo@gmail.com>
2020-12-28 14:31:22 -0300

    Loops parte 1 - While

---
+++ loops_while.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Curso PHP</title>
+	</head>
+	<body>
+		
+		<?php  
+
+            $num = 1;
+            //operadores comparação / lógicos
+            echo '-- Início do loop --<br/>';
+            while($num < 10) {
+                
+                $num += 1; //critério de parada
+
+                if($num == 2 || $num == 6) {
+                    continue;
+                }
+
+                echo "$num <br/>";
+
+                /*
+                if($num > 100) {
+                    break;
+                }
+                */
+            }
+            echo '-- Fim do loop --';
+
+		?>
+
+	</body>
+</html>
